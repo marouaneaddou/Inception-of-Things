c880ffc3a9c0198cb8aa48c297807fa2eb10d23c
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-02-01 09:06:11 +0100

    Configure Ingress for app1, app2, and app3 services

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -1,5 +1,5 @@
 apt-get update 
-apt-get upgrade -y
+# apt-get upgrade -y
 apt-get install curl -y
 mkdir -p /home/vagrant/.kube
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig  /home/vagrant/.kube/config --write-kubeconfig-mode=644" sh -s
