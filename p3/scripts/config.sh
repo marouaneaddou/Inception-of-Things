6ed2bce81e11cbd004db6c4d5784482615c4f6e1
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-02-20 17:32:23 +0000

    Add config file for installing k3d and setting up cluster with ArgoCD

---
+++ p3/scripts/config.sh
@@ -0,0 +1,16 @@
+
+k3d &> /dev/null
+if [[ $? -eq 1 ]]; then
+    echo "is installed befaore"
+else 
+    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+fi
+
+k3d cluster create inception-of-things -p "8888:80@server:0"
+
+kubectl create namespace argocd
+
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl wait --for=condition=ready pods --all -n argocd
+kubectl apply -f ../application.yaml
