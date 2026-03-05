7934652ff36725f169ef08edfd375015cd25265f
mbaanni <hamide185@gmail.com>
2026-03-05 13:42:13 +0000

    changed path

--- p2/scripts/deploy.sh
+++ p2/scripts/deploy.sh
@@ -1,8 +1,8 @@
 
-kubectl apply -f /home/vagrant/deploy/apps/app1/deployment.yaml \
-    -f /home/vagrant/deploy/apps/app1/service.yaml \
-    -f /home/vagrant/deploy/apps/app2/deployment.yaml \
-    -f /home/vagrant/deploy/apps/app2/service.yaml \
-    -f /home/vagrant/deploy/apps/app3/deployment.yaml \
-    -f /home/vagrant/deploy/apps/app3/service.yaml \
-    -f /home/vagrant/deploy/ingress.yaml
+kubectl apply -f /home/vagrant/deploy/confs/apps/app1/deployment.yaml \
+    -f /home/vagrant/deploy/confs/apps/app1/service.yaml \
+    -f /home/vagrant/deploy/confs/apps/app2/deployment.yaml \
+    -f /home/vagrant/deploy/confs/apps/app2/service.yaml \
+    -f /home/vagrant/deploy/confs/apps/app3/deployment.yaml \
+    -f /home/vagrant/deploy/confs/apps/app3/service.yaml \
+    -f /home/vagrant/deploy/confs/ingress.yaml
