94154e6e014934f4d01309558c26fe55ff7f0209
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-06 18:05:33 +0000

    changed k3s conf

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -3,9 +3,8 @@
 apk update
 #apt-get upgrade -y
 apk add --no-cache curl bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig  /home/vagrant/vagrant_data --write-kubeconfig-mode=644" sh -s
 #cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
 
-sudo cp /var/lib/rancher/k3s/server/node-token /home/vagrant/vagrant_data/token
 #cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 #chown -R vagrant:vagrant /home/vagrant/.ssh
