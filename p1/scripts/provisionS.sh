15b9af6563f14c2c9d63fe0c5045725e9001521c
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-03-06 21:20:34 +0000

    Fix problem in creation  of the master node

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -3,8 +3,11 @@
 apk update
 #apt-get upgrade -y
 apk add --no-cache curl bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig  /home/vagrant/vagrant_data --write-kubeconfig-mode=644" sh -s
-#cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
 
-#cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-#chown -R vagrant:vagrant /home/vagrant/.ssh
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+  sleep 2
+done
+
+cp /var/lib/rancher/k3s/server/node-token /home/vagrant/vagrant_data/token
+chmod 644 /home/vagrant/vagrant_data/token
--- p1/scripts/provisionSW.sh
+++ p1/scripts/provisionSW.sh
@@ -3,8 +3,5 @@
 apk update
 #apk upgrade -y
 apk add --no-cache curl bash 
-#cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
-#cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-#chown -R vagrant:vagrant /home/vagrant/.ssh
 until curl -k https://192.168.56.110:6443; do sleep 1; done
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" K3S_TOKEN="$(cat /home/vagrant/vagrant_data/token)" sh -s
