ac486a702f015405c6e7f9d87cef63e2356d68b8
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-06 16:55:25 +0000

    changed distro and removed upgrade

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -1,5 +1,5 @@
 apt-get update 
-apt-get upgrade -y
+#apt-get upgrade -y
 apt-get install curl -y
 mkdir -p /home/vagrant/.kube
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig  /home/vagrant/.kube/config --write-kubeconfig-mode=644" sh -s
