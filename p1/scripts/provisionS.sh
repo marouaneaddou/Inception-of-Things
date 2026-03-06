7ce0b6c1d003c969dcc03069eb8672047a56d871
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-06 17:42:09 +0000

    changed distro to alpine and script command

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-apt-get update -y
+apk update -y
 #apt-get upgrade -y
-apt-get install curl -y
+apk add --no-cache curl bash
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
 #cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
+sudo systemctl enable k3s
+sudo systemctl start k3s
 cp /var/lib/rancher/k3s/server/node-token /home/vagrant/vagrant_data/token
 #cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 #chown -R vagrant:vagrant /home/vagrant/.ssh
--- p1/scripts/provisionSW.sh
+++ p1/scripts/provisionSW.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-apt-get update -y
-#apt-get upgrade -y
-apt-get install curl -y
+apk update -y
+#apk upgrade -y
+apk add --no-cache curl bash
 #cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
 #cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 #chown -R vagrant:vagrant /home/vagrant/.ssh
 until curl -k https://192.168.56.110:6443; do sleep 1; done
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" K3S_TOKEN="$(cat /home/vagrant/vagrant_data/token)" sh -s
+sudo systemctl enable k3s-agent
+sudo systemctl start k3s-agent
