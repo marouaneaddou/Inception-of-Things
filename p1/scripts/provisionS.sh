e30b66d1c132362ff243e86ba510018b015fe393
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-06 17:55:14 +0000

    removed openrc

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -2,10 +2,9 @@
 
 apk update -y
 #apt-get upgrade -y
-apk add --no-cache curl bash openrc
+apk add --no-cache curl bash
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
 #cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
-rc-update add k3s default
 /etc/init.d/k3s start
 sleep 2
 cp /var/lib/rancher/k3s/server/node-token /home/vagrant/vagrant_data/token
