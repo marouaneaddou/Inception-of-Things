109f301ba8aad3fa7f5ffeec0c4f5395e674ff63
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-06 17:50:40 +0000

    removed systemctl command

--- p1/scripts/provisionS.sh
+++ p1/scripts/provisionS.sh
@@ -2,11 +2,12 @@
 
 apk update -y
 #apt-get upgrade -y
-apk add --no-cache curl bash
+apk add --no-cache curl bash openrc
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -s
 #cp /home/vagrant/vagrant_data/id_rsa /home/vagrant/.ssh/id_rsa
-sudo systemctl enable k3s
-sudo systemctl start k3s
+rc-update add k3s default
+/etc/init.d/k3s start
+sleep 2
 cp /var/lib/rancher/k3s/server/node-token /home/vagrant/vagrant_data/token
 #cat /home/vagrant/vagrant_data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 #chown -R vagrant:vagrant /home/vagrant/.ssh
