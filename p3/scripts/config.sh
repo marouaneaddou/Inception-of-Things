283550c9deda278c77acbb35bf2acbc7036db1a0
Mohamed Baanni <mbaanni@c1r7p6.1337.ma>
2026-03-07 16:37:39 +0000

    changed file path

--- p3/scripts/config.sh
+++ p3/scripts/config.sh
@@ -14,7 +14,7 @@ kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --for=condition=ready pods --all -n argocd
-kubectl apply -f ../application.yaml
+kubectl apply -f ../confs/application.yaml
 
 echo "kubectl get secret argocd-initial-admin-secret -n argocd -o yaml"
 echo "" | base64 --decode
