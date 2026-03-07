894bd0ad38af13ac186715161694a8933a7a6d7e
mbaanni <hamide185@gmail.com>
2026-03-07 18:06:35 +0000

    path argoconf

--- bonus/scripts/argocd-config.sh
+++ bonus/scripts/argocd-config.sh
@@ -12,6 +12,6 @@ kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --for=condition=ready pods --all -n argocd
-kubectl apply -f ../confs/application.yaml
+kubectl apply -f ../confs/argocd-application.yaml
 
 echo "kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode"
